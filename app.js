2309de311b521c4af4462d81c48e8c52b8a514fe
C'Aira Shields <cairacshields@gmail.com>
2018-12-22 17:17:38 -0600

    changes to port

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var bodyParser = require('body-parser');
 // See your keys here: https://dashboard.stripe.com/account/apikeys
 var stripe = require("stripe")("sk_test_km8Vo3mjUEOtkU2SaizC6QmR");
 var app = express();
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080
 app.use(bodyParser());
 
 
@@ -11,6 +14,6 @@ app.get("/url", (req, res, next) => {
  res.json(["Tony","Lisa","Michael","Ginger","Food"]);
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
  console.log("Server running on port 3000");
 });
