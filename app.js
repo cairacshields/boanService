675a78ebbb01830305ef855e972275fc35892513
C'Aira Shields <cairacshields@gmail.com>
2018-12-22 18:19:44 -0600

    add stripe stuff

--- app.js
+++ app.js
@@ -9,6 +9,42 @@ var app = express();
 var port = process.env.PORT || 8080
 app.use(bodyParser());
 
+//Need to send the following items in the request body on the client side
+/*
+1. stripeToken
+2. user email
+3. charge amount in cents 
+
+*/
+ app.post('/charge', function(req, res) {
+    var stripeToken = req.body.stripeToken;
+
+ 	(async function() {
+ 	  // Create a Customer:
+ 	  const customer = await stripe.customers.create({
+ 	    source: stripeToken,
+ 	    email: req.body.email,
+ 	  });
+
+     var charge = stripe.charges.create({
+         amount: req.body.amount,
+     	currency: 'usd',
+     	customer: customer.id
+     }, 
+     function(err, charge) {
+         if (err && err.type === 'StripeCardError') {
+             console.log("The card has been declined");
+             res.write("The card has been declined" + err)
+             res.send("The card has been declined" + err)
+            
+         }else if(err){
+			res.write("The card has been declined" + err)
+             res.send("The card has been declined" + err)
+         }
+     });
+
+     // YOUR CODE: Save the customer ID and other info in a database for later.
+     
 
 app.get("/url", (req, res, next) => {
  res.json(["Tony","Lisa","Michael","Ginger","Food"]);
