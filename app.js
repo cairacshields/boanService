4aa7494a625d4163ff52e460a25ea4024ecd9fef
C'Aira Shields <cairacshields@gmail.com>
2018-12-25 14:46:07 -0600

    add stripe code

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require("express"),
 	bodyParser = require('body-parser'),
+ 	__async = require('asyncawait/async'),
+ 	__await = require('asyncawait/await'),
  	stripe = require("stripe")("sk_test_km8Vo3mjUEOtkU2SaizC6QmR"),
  	admin = require('firebase-admin'),
  	serviceAccount = require('./serviceAccountKey.json');
@@ -23,7 +25,7 @@ ref.once("value", function(snapshot) {
   console.log("The read failed: " + errorObject.code);
 });
 
-var refUsers = db.ref("users/"); //base db reference to the user node... need to chain the userId 
+//var refUsers = db.ref("users/"); //base db reference to the user node... need to chain the userId 
 
 // app.use(bodyParser());
 
@@ -32,57 +34,53 @@ var refUsers = db.ref("users/"); //base db reference to the user node... need to
 1. stripeToken
 2. user email
 3. charge amount in cents 
-
 */
-//  app.post('/charge', function(req, res) {
-//     var stripeToken = req.body.stripeToken;
-
-//  	(async function() {
-//  	  // Create a Customer:
-//  	  const customer = await stripe.customers.create({
-//  	    source: stripeToken,
-//  	    email: req.body.email,
-//  	  });
-
-//      var charge = stripe.charges.create({
-//          amount: req.body.amount,
-//      	currency: 'usd',
-//      	customer: customer.id
-//      }, 
-//      function(err, charge) {
-//          if (err && err.type === 'StripeCardError') {
-//              console.log("The card has been declined");
-//              res.write("The card has been declined" + err)
-//              res.send("The card has been declined" + err)
-            
-//          }else if(err){
-// 			res.write("The card has been declined" + err)
-//              res.send("The card has been declined" + err)
-//          }
-//      }); 
-//   });
-// });
-     // YOUR CODE: Save the customer ID and other info in a database for later.
-     
 app.post("/charge", function(req, res){
-	//val stripeToken = req.body.stripeToken;
-	//val userEmail = req.body.email;
+	var stripeToken = req.body.stripeToken;
+	var userEmail = req.body.email;
 	var userId = req.body.userId;
-	//val centAmount = req.body.amount;
+	var centAmount = req.body.amount;
 
 	var refUsers = db.ref("users/"+userId);
 
-	refUsers.on("value", function(snapshot) {
-	  var user = snapshot.val();
-	  res.write(user.email);
+	(__async function() {
+	 	  // Create a Customer:
+	   var customer = __await stripe.customers.create({
+	 	    source: stripeToken,
+	 	    email: userEmail
+	 	 });
 
-	}, function (errorObject) {
+ 	   var charge = __await stripe.charges.create({
+	        amount: centAmount,
+	     	currency: 'usd',
+	     	customer: customer.id
+	     }, 
+	     function(err, charge) {
+	         if (err && err.type === 'StripeCardError') {
+	             console.log("The card has been declined");
+	             res.write("The card has been declined" + err)
+	             res.send("The card has been declined" + err)
+	            
+	         }else if(err){
+				res.write("The card has been declined" + err)
+	             res.send("The card has been declined" + err)
+	         }
+	     }); 
 
-	  console.log("The read failed: " + errorObject.code);
-	  res.write(errorObject.code);
-	});
+ 	   refUsers.on("value", function(snapshot) {
+		//get the user and update the customerId
+		  var user = snapshot.val();
+		  refUsers.child("customerId").set(customer.id)
+		  res.write(user.customerId);
 
-	res.write("Hello, World " + req.body.phrase);
+		}, function (errorObject) {
+
+		  console.log("The read failed: " + errorObject.code);
+		  res.write(errorObject.code);
+		});
+
+ 	});
+
 });     
 
 app.get("/", ( req, res, next) => {
