f9a242a545baffcd6e3d16df2d0da692a4adce2f
C'Aira Shields <cairacshields@gmail.com>
2018-12-26 13:39:35 -0600

    getting both dates

--- app.js
+++ app.js
@@ -168,14 +168,14 @@ app.get("/", ( req, res, next) => {
 	var refTermsAgreements = db.ref("termsAgreements");
 
 	//This is going to use the DB reference to grab each termsAgreement once. Inside here, we can check the 'accepted' value
-	refTermsAgreements.once("value", function(data, prevChildKey) {
+	refTermsAgreements.once("value", function(data) {
   		//if(data.accepted != false){
   			//The terms agreement has been accepted and the lender was charged. 
   			//Time to check the repay date, against today's date.
   			//var miliDate = new Date(data.repayDate.getTime());
   			//var repayDate = new Date(data.repayDate.time);
-  			res.json(prevChildKey);
-  			console.log(prevChildKey);
+  			res.json(data.val().child().val());
+  			console.log(data.val().child().val());
 
   		//}else{
   			//The terms agreement hasn't been accepted yet 
