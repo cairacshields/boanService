c32cfe0553bfe24dd356970a79e69def60e51540
C'Aira Shields <cairacshields@gmail.com>
2018-12-26 13:23:19 -0600

    getting both dates

--- app.js
+++ app.js
@@ -174,7 +174,7 @@ app.get("/", ( req, res, next) => {
   			//Time to check the repay date, against today's date.
   			//var miliDate = new Date(data.repayDate.getTime());
   			//var repayDate = new Date(data.repayDate.time);
-  			res.json(data.size();
+  			res.json(data.size());
 
   		//}else{
   			//The terms agreement hasn't been accepted yet 
