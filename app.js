dffdae7f24579aa9be19acb4d2ed42330a625525
C'Aira Shields <cairacshields@gmail.com>
2019-03-07 10:29:58 -0600

    testing server changes

--- app.js
+++ app.js
@@ -480,7 +480,7 @@ app.get("/testing", (req, res, next) => {
 		  			console.log("Borrow request removed due to non acceptence of terms agreements, or not receiving any terms agreements.");
 		  		}
 		  	}else{
-				console.log("Borrow request not removed on line 475, date isn't the same. " + todaysDate.getUTCDate()
+				console.log("Borrow request not removed on line 475, date isn't the same. " + date.getUTCDate()
 					+ " " + repayDate.getUTCDate());
 		  	}
 		});
